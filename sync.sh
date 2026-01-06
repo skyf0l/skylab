03df6335aeb56941f4b540e45cf5c8df07b8677f
Skyf0l <59019720+skyf0l@users.noreply.github.com>
2026-01-06 15:54:26 +1100

    feat: deploy root argoCD app on sync

--- sync.sh
+++ sync.sh
@@ -4,9 +4,12 @@ set -eE
 
 repoDir=$(git rev-parse --show-toplevel)
 
-repoName="gitops"
-giteaAdminUsername="admin"
+giteaRepoName="gitops"
+giteaAdminUsername="gitea_admin"
+giteaAdminEmail="gitea@local.domain"
 giteaAdminPassword="admin"
+
+rootApplicationPath="${repoDir}/clusters/vagrant/application.yml"
 
 portForwardPort=3000
 kubectl port-forward --namespace=gitea svc/gitea-http ${portForwardPort}:3000 &
@@ -19,6 +22,7 @@ trap 'clean' SIGINT SIGTERM
 
 # Wait for port-forward to be established
 while ! nc -z localhost ${portForwardPort}; do
+    echo "Waiting for Gitea port-forward to be established..."
     sleep 1
 done
 
@@ -29,15 +33,15 @@ done
     --request POST \
     --user "${giteaAdminUsername}:${giteaAdminPassword}" \
     --header "Content-Type: application/json" \
-    --data '{"name":"'${repoName}'", "default_branch":"main", "private":true}' \
+    --data '{"name":"'${giteaRepoName}'", "default_branch":"main", "private":true}' \
     "http://localhost:${portForwardPort}/api/v1/user/repos"
     
     rsync -a ${repoDir}/ ${tempDir} --exclude ".git" --filter=':- .gitignore'
     cd "${tempDir}"
     git init
-    git remote add origin "http://${giteaAdminUsername}:${giteaAdminPassword}@localhost:${portForwardPort}/${giteaAdminUsername}/${repoName}.git"
+    git remote add origin "http://${giteaAdminUsername}:${giteaAdminPassword}@localhost:${portForwardPort}/${giteaAdminUsername}/${giteaRepoName}.git"
     git add . || true
-    git config user.email "gitea@local.domain"
+    git config user.email "${giteaAdminEmail}"
     git config user.name "${giteaAdminUsername}"
     git commit -m "Initial commit" || true
     git push origin HEAD:main --force
@@ -45,3 +49,20 @@ done
 )
 
 kill ${giteaPortForwardPid} 2>/dev/null || true
+
+cat<<EOF | kubectl apply --namespace=argocd -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ${giteaRepoName}
+  namespace: argocd
+  labels:
+    argocd.argoproj.io/secret-type: repository
+stringData:
+  type: git
+  url: http://gitea-http.gitea.svc.cluster.local:${portForwardPort}/${giteaAdminUsername}/${giteaRepoName}
+  password: ${giteaAdminPassword}
+  username: ${giteaAdminUsername}
+EOF
+
+kubectl apply --namespace=argocd -f "${rootApplicationPath}"
